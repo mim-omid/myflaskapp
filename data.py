d0ccaa85322651cdd9e0286ed5105bd1384335d4
mim-omid <mr.mim.omid@gmail.com>
2018-11-02 12:33:51 +0330

    init

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id': 1,
+            'title':'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quia esse, magni voluptate necessitatibus ipsum dolorem nihil alias debitis voluptatem expedita ipsa recusandae, praesentium totam laborum ullam illo vel voluptas!',
+            'author':'Mojtaba Omid',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quia esse, magni voluptate necessitatibus ipsum dolorem nihil alias debitis voluptatem expedita ipsa recusandae, praesentium totam laborum ullam illo vel voluptas!',
+            'author':'John Doe',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quia esse, magni voluptate necessitatibus ipsum dolorem nihil alias debitis voluptatem expedita ipsa recusandae, praesentium totam laborum ullam illo vel voluptas!',
+            'author':'Mojtaba Omid',
+            'create_date':'04-25-2017'
+        },
+    ]
+    return articles
