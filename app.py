8c072896945dd945532ce97872e29c80bb5af1d1
mim-omid <mr.mim.omid@gmail.com>
2018-11-11 19:55:46 +0330

    connect to database MySQL

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='123456'
+app.config['MYSQL_DB']='myflaskapp'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+
+#INIT MYSQL
+
+mysql = MySQL(app)
+
 Articles = Articles()
 
 @app.route('/')
@@ -38,8 +49,29 @@ class RegisterForm(Form):
 def register():
     form=RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        name =form.name.data
+        email = form.email.data
+        username=form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name,email, username, password))
+
+        #Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('You are now registered and can log in','success')
+        
+        return redirect(url_for('login'))
+        
     return render_template('register.html',form=form)
 
 if __name__ =="__main__":
+    app.secret_key='secret123'
     app.run(debug=True)
