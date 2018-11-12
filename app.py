3378df54882a694c8118c71f3f84af8ccc1c49e3
mim-omid <mr.mim.omid@gmail.com>
2018-11-12 17:48:06 +0330

    add login page

--- app.py
+++ app.py
@@ -72,6 +72,45 @@ def register():
         
     return render_template('register.html',form=form)
 
+# User Login
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        #Get form fields
+        username=request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s",[username])
+
+        if result>0:
+            #Get Stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate,password):
+                # Passed
+                session['logged_in']=True
+                session['username']=username
+
+                flash('You are now logged in','success')
+                #return redirect(url_for('dashboard'))
+            else:
+                #app.logger.info("Password not Matched")
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html',error=error)
+
+    return render_template('login.html')
+
 if __name__ =="__main__":
     app.secret_key='secret123'
     app.run(debug=True)
