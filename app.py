0f64dcbbdfb01abd2ae6e64bbdb50801b8064a82
mim-omid <mr.mim.omid@gmail.com>
2018-11-13 19:43:51 +0330

    show articles in articles nav

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request,flash, redirect,url_for,session,logging
-from data import Articles
+#from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -18,7 +18,7 @@ app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 mysql = MySQL(app)
 
-Articles = Articles()
+#Articles = Articles()
 
 #Index
 @app.route('/')
@@ -33,12 +33,35 @@ def about():
 # Articles
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles= Articles)
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result>0:
+        return render_template('/articles.html', articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template('/articles.html',msg=msg)
+    
+    #Close Connection
+    cur.close()
+    #return render_template('articles.html', articles= Articles)
 
 # Single Article
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
+    #Create cursor
+    cur =mysql.connection.cursor()
+
+    #Get articles
+    result = cur.execute("SELECT * FROM articles WHERE id=%s",[id])
+    article = cur.fetchone()
+
+    return render_template('article.html', article=article)
 
 # Register form class
 class RegisterForm(Form):
@@ -141,7 +164,22 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-    return render_template('/dashboard.html')
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result>0:
+        return render_template('/dashboard.html', articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template('/dashboard.html',msg=msg)
+    
+    #Close Connection
+    cur.close()
 
 # Article Form Class
 class ArticleForm(Form):
